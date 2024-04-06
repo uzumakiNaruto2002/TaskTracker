f0fcbb02c65169d9c6083e03134dc9ab3bb07ed0
uzumakiNaruto2002 <138820428+uzumakiNaruto2002@users.noreply.github.com>
2024-04-06 12:22:43 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-      <p>Made by <a href='https://leetcode.com/1821_sank/' target='_blank' rel="noreferrer" className='link'>Sankalp Jain</a></p>
+      <p>Made by <a href='https://leetcode.com/Monkey_D_Luffy_2002/' target='_blank' rel="noreferrer" className='link'>Rupesh Maheshwari</a></p>
     </div>
   )
 }
